b434c35cdad9f620e6567d9a0d09c104d3258425
William Kennedy <bill@ardanlabs.com>
2022-01-20 09:15:29 -0500

    adding documentation for reading and watching material

---
+++ foundation/node/doc.go
@@ -0,0 +1,13 @@
+/*
+READING AND NOTES
+
+- Articles
+[Transparent Logs for Skeptical Clients](https://research.swtch.com/tlog) - Russ Cox
+
+- Books
+[Build a blockchain from scratch](https://web3.coach/) - Lukas Lukae
+
+-- Video
+[Is It Possible To Get The Same 24 BIP39 Seed Words?](https://www.youtube.com/watch?v=hjRntYh0ot8) - John Chow
+*/
+package node
