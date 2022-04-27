6c42d354f5caaedb05d9c0aefdda1071a8a02458
Ole Bulbuk <ole@bulbuk.de>
2022-04-27 15:39:03 +0200

    Make dynamic HTML+SVG readable

--- app/services/viewer/assets/scripts/main.js
+++ app/services/viewer/assets/scripts/main.js
@@ -6,29 +6,13 @@ socket.addEventListener('open', function (event) {
 
 socket.addEventListener('message', function (event) {
     msgBlock = document.getElementById("msg-block");
-    
-    // Create arrow using SVG
-    var svg = getNode('svg', { height:32 });
-
-    var line1 = getNode('line', { x1:220, x2:220,  y1:0, y2:32, stroke:'rgb(0,0,0)', 'stroke-opacity':'1.0', 'stroke-width':'2.5'});
-    var line2 = getNode('line', { x1:212, x2:220,  y1:24, y2:32, stroke:'rgb(0,0,0)', 'stroke-opacity':'1.0', 'stroke-width':'2.5'});
-    var line3 = getNode('line', { x1:228, x2:220,  y1:24, y2:32, stroke:'rgb(0,0,0)', 'stroke-opacity':'1.0', 'stroke-width':'2.5'});
-    svg.appendChild(line1);
-    svg.appendChild(line2);
-    svg.appendChild(line3);
-    msgBlock.appendChild(svg);
-
-    // Create Text block
-    const newMsg = document.createElement("div");
-    newMsg.setAttribute("class", "block-class");
-    const node = document.createTextNode(event.data);
-    newMsg.appendChild(node);
-    msgBlock.appendChild(newMsg);
+	text = event.data;
+    msgBlock.innerHTML += `
+	    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 0 300 34">
+	        <line stroke="rgb(0,0,0)" stroke-opacity="1.0" stroke-width="2.5" x1="220" y1="0" x2="220" y2="32"/>
+	        <line stroke="rgb(0,0,0)" stroke-opacity="1.0" stroke-width="2.5" x1="212" y1="24" x2="220" y2="32"/>
+	        <line stroke="rgb(0,0,0)" stroke-opacity="1.0" stroke-width="2.5" x1="228" y1="24" x2="220" y2="32"/>
+	    </svg>
+		<div class="block-class">${text}</div>
+`;
 });
-
-function getNode(n, v) {
-    n = document.createElementNS("http://www.w3.org/2000/svg", n);
-    for (var p in v)
-        n.setAttributeNS(null, p, v[p]);
-    return n
-}
