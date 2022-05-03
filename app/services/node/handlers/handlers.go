da7f1e7136bee8a17dfe0b98c67f0f91cadad071
Ole Bulbuk <ole@bulbuk.de>
2022-05-03 15:47:28 +0200

    Draw boxes in viewer (#11)
    * Draw text boxes and arrows in viewer app
    
    * Make dynamic HTML+SVG readable
    
    * Send block updates to the viewer
    
    * Refactor Viewer Javascript
    
    * Show only blocks as table data
    
    * Add state root
    
    * Start to read old blocks
    
    * adding cors on private endpoints
    
    * Update node ports on docker-compose.yml file to match makefile
    
    * Fix CORS issue in private endpoint
    
    * Append existing blocks to viewer HTML
    
    * Extract logic to append blocks on Viewer app into a new method
    
    * Prevent double blocks
    
    Co-authored-by: Marcelo Milera <marcelo.milera@x-team.com>
    Co-authored-by: William Kennedy <bill@ardanlabs.com>

--- app/services/node/handlers/handlers.go
+++ app/services/node/handlers/handlers.go
@@ -68,8 +68,17 @@ func PrivateMux(cfg MuxConfig) http.Handler {
 		mid.Logger(cfg.Log),
 		mid.Errors(cfg.Log),
 		mid.Metrics(),
+		mid.Cors("*"),
 		mid.Panics(),
 	)
+
+	// Accept CORS 'OPTIONS' preflight requests if config has been provided.
+	// Don't forget to apply the CORS middleware to the routes that need it.
+	// Example Config: `conf:"default:https://MY_DOMAIN.COM"`
+	h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		return nil
+	}
+	app.Handle(http.MethodOptions, "", "/*", h, mid.Cors("*"))
 
 	// Load the v1 routes.
 	v1.PrivateRoutes(app, v1.Config{
--- app/services/viewer/assets/scripts/main.js
+++ app/services/viewer/assets/scripts/main.js
@@ -1,9 +1,130 @@
-const socket = new WebSocket('ws://localhost:8080/v1/events');
+function connect(wsUrl, httpUrl, id) {
+    let blockHashes = new Set();
+    let lastBlockHash = "";
+    const handleNewBlock = function(hash, block) {
+        if (blockHashes.size === 0) {
+            document.getElementById(`first-msg${id}`).innerHTML = getBlockTable(hash, block);
+            blockHashes.add(hash);
+            lastBlockHash = hash;
+            return;
+        }
+        if (blockHashes.has(hash)) {
+            return;
+        }
+        if (block.prev_block_hash === lastBlockHash) {
+            addArrow(id);
+        }
+        addBlock(id, hash, block);
+        blockHashes.add(hash);
+        lastBlockHash = hash;
+    }
 
-socket.addEventListener('open', function (event) {
-    document.getElementById("msg").innerHTML = "connection open";
-});
+    const reqListener = function() {
+        var responseJson = JSON.parse(this.responseText);
+        msgBlock = document.getElementById(`msg-block${id}`);
+        for (i = 0; i < responseJson.length; i++) {
+            handleNewBlock(responseJson[i].hash, responseJson[i].block);
+        }
+    }
 
-socket.addEventListener('message', function (event) {
-    document.getElementById("msg").innerHTML += "<BR />" + event.data;
-});
+    let socket = new WebSocket(wsUrl);
+    socket.onopen = function() {
+        document.getElementById(`first-msg${id}`).innerHTML = `Node ${id}: Connection open`;
+
+        var oReq = new XMLHttpRequest();
+        oReq.addEventListener("load", reqListener.bind(oReq, id), false);
+        oReq.open("GET", httpUrl);
+        oReq.send();
+    };
+  
+    socket.onmessage = function(event) {
+        const blockMsgStart = 'viewer: block: ';
+        let text = event.data;
+        if (!text.startsWith(blockMsgStart)) {
+            return;
+        }
+        text = text.substring(blockMsgStart.length);
+        let block = JSON.parse(text);
+        handleNewBlock(block.hash, block.header);
+    };
+  
+    socket.onclose = function(event) {
+        console.log('Socket is closed. Reconnect will be attempted in 1 second.', event.reason);
+        document.getElementById(`first-msg${id}`).innerHTML = `Node ${id}: Connecting...`;
+        setTimeout(function() {
+            connect(wsUrl, httpUrl, id);
+        }, 1000);
+    };
+  
+    socket.onerror = function(err) {
+      console.error('Socket encountered error: ', err.message, 'Closing socket');
+      socket.close();
+    };
+}
+
+function getBlockTable(hash, block) {
+    return `
+            <table>
+                <tr>
+                    <td class="key">Own Hash:</td>
+                    <td colspan="5" class="value">${hash}</td>
+                </tr>
+                <tr>
+                    <td class="key">Previous Hash:</td>
+                    <td colspan="5" class="value">${block.prev_block_hash}</td>
+                </tr>
+                <tr>
+                    <td class="key">Block Number:</td>
+                    <td class="value">${block.number}</td>
+                    <td class="key">Mining Difficulty:</td>
+                    <td class="value">${block.difficulty}</td>
+                    <td class="key">Mining Reward:</td>
+                    <td class="value">${block.mining_reward}</td>
+                </tr>
+                <tr>
+                    <td class="key">Timestamp:</td>
+                    <td colspan="2" class="value">${block.timestamp}</td>
+                    <td class="key">Nonce:</td>
+                    <td colspan="2" class="value">${block.nonce}</td>
+                </tr>
+                <tr>
+                    <td class="key">Beneficiary:</td>
+                    <td colspan="5" class="value">${block.beneficiary}</td>
+                </tr>
+                <tr>
+                    <td class="key">Transaction Root:</td>
+                    <td colspan="5" class="value">${block.trans_root}</td>
+                </tr>
+                <tr>
+                    <td class="key">State Root:</td>
+                    <td colspan="5" class="value">${block.state_root}</td>
+                </tr>
+            </table>
+    `;
+}
+
+function addBlock(id, hash, block) {
+    msgBlock = document.getElementById(`msg-block${id}`);
+    blockTable = getBlockTable(hash, block)
+    msgBlock.innerHTML += `
+        <div class="block-class">
+            ${blockTable}
+        </div>
+    `;
+    msgBlock.scrollTop = msgBlock.scrollHeight;
+}
+
+function addArrow(id) {
+    msgBlock = document.getElementById(`msg-block${id}`);
+    msgBlock.innerHTML += `
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 0 300 34">
+            <line stroke="rgb(0,0,0)" stroke-opacity="1.0" stroke-width="2.5" x1="220" y1="0" x2="220" y2="32"/>
+            <line stroke="rgb(0,0,0)" stroke-opacity="1.0" stroke-width="2.5" x1="212" y1="24" x2="220" y2="32"/>
+            <line stroke="rgb(0,0,0)" stroke-opacity="1.0" stroke-width="2.5" x1="228" y1="24" x2="220" y2="32"/>
+        </svg>
+    `;
+}
+
+connect('ws://localhost:8080/v1/events', 'http://localhost:9080/v1/node/block/list/1/latest', '1');
+connect('ws://localhost:8180/v1/events', 'http://localhost:9180/v1/node/block/list/1/latest', '2');
+connect('ws://localhost:8280/v1/events', 'http://localhost:9280/v1/node/block/list/1/latest', '3');
