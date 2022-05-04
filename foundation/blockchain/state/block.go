e0ad5b06c56c9f66f0b746acced0a5eec76f0f2a
Ole Bulbuk <ole@bulbuk.de>
2022-05-04 18:47:48 +0200

    Make sending block over WS more consistent

--- foundation/blockchain/state/block.go
+++ foundation/blockchain/state/block.go
@@ -141,15 +141,11 @@ func (s *State) validateUpdateDatabase(block database.Block) error {
 // blockEvent provides a specific event about a new block in the chain for
 // application specific support.
 func (s *State) blockEvent(block database.Block) {
-	blockHeaderJSON, err := json.Marshal(block.Header)
+	blockData := database.NewBlockData(block)
+	data, err := json.Marshal(blockData)
 	if err != nil {
-		blockHeaderJSON = []byte(fmt.Sprintf("%q", err.Error()))
+		data = []byte(fmt.Sprintf("{error: %q}", err.Error()))
 	}
 
-	blockTransJSON, err := json.Marshal(block.MerkleTree.Values())
-	if err != nil {
-		blockTransJSON = []byte(fmt.Sprintf("%q", err.Error()))
-	}
-
-	s.evHandler(`viewer: block: {"hash":%q,"header":%s,"trans":%s}`, block.Hash(), string(blockHeaderJSON), string(blockTransJSON))
+	s.evHandler("viewer: block: %s" + string(data))
 }
