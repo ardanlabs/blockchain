80e56ab9f9c9d2b1674233de750e768f3a1f5ab4
William Kennedy <bill@ardanlabs.com>
2022-06-07 12:33:30 -0400

    refactoring

--- foundation/blockchain/state/state_test.go
+++ foundation/blockchain/state/state_test.go
@@ -2,9 +2,7 @@ package state_test
 
 import (
 	"context"
-	"fmt"
-	"math/rand"
-	"strings"
+	"errors"
 	"testing"
 	"time"
 
@@ -13,106 +11,107 @@ import (
 	"github.com/ardanlabs/blockchain/foundation/blockchain/peer"
 	"github.com/ardanlabs/blockchain/foundation/blockchain/state"
 	"github.com/ardanlabs/blockchain/foundation/blockchain/storage/memory"
-	"github.com/ardanlabs/blockchain/foundation/events"
 	"github.com/ardanlabs/blockchain/foundation/logger"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 // ============================ TESTS CASES ===================================
 
-// Test_MineAndSyncBlock - Simple happy path. We do a transaction, mine a
-// block and offer it to another miner - no issues should be found.
+// Test_MineAndSyncBlock is the simple happy path. We do a transaction, mine a
+// block and offer it to another miner. No issues should be found.
 func Test_MineAndSyncBlock(t *testing.T) {
-
 	log, err := logger.New("TEST")
 	if err != nil {
-		t.Error(err)
-		t.FailNow()
+		t.Fatalf("Error constructing logger: %v", err)
 	}
 	defer log.Sync()
 
-	state1 := newMiner(t, MINER1_ECDSA)
-	state2 := newMiner(t, MINER2_ECDSA)
+	node1 := newNode(MINER1_PRIVATEKEY, t)
+	node2 := newNode(MINER2_PRIVATEKEY, t)
 
-	txOpts := newSignedTx(t)
-
-	state1.UpsertWalletTransaction(txOpts)
-
-	// Let them interact.
-	blk, err := state1.MineNewBlock(context.Background())
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
+	tx := database.Tx{
+		ChainID: CHAIN_ID,
+		Nonce:   1,
+		ToID:    KENNEDY_ACCOUNTID,
+		Value:   1,
+		Tip:     0,
+		Data:    nil,
 	}
 
-	err = state2.ProcessProposedBlock(blk)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
+	signedTx := newSignedTx(tx, JACK_PRIVATEKEY, t)
+	if err := node1.UpsertWalletTransaction(signedTx); err != nil {
+		t.Fatalf("Error upserting wallet transaction: %v", err)
 	}
 
-	log.Info("Done")
+	blk, err := node1.MineNewBlock(context.Background())
+	if err != nil {
+		t.Fatalf("Error mining new block: %v", err)
+	}
+
+	err = node2.ProcessProposedBlock(blk)
+	if err != nil {
+		t.Fatalf("Error proposing new block: %v", err)
+	}
 }
 
-// Test_MineAndSyncBlocksNotRespectingOrder - in this scenario we will create
-// 2 Miners, mine some blocks on Miner 1, and then, provide the blocks to Miner 2,
-// but some blocks will be missing and we expect to see it raising a database.ErrChainForked.
-func Test_MineAndForceRescynError(t *testing.T) {
-
+// Test_MineAndErrChainForkedDetection will create 2 nodes, mine some
+// blocks on node 1, then provide the blocks to node 2. FINISH COMMENT!
+func Test_MineAndErrChainForkedDetection(t *testing.T) {
 	log, err := logger.New("TEST")
 	if err != nil {
-		t.Error(err)
-		t.FailNow()
+		t.Fatalf("Error constructing logger: %v", err)
 	}
-
 	defer log.Sync()
 
-	state1 := newMiner(t, MINER1_ECDSA)
-	state2 := newMiner(t, MINER2_ECDSA)
+	node1 := newNode(MINER1_PRIVATEKEY, t)
+	node2 := newNode(MINER2_PRIVATEKEY, t)
 
-	// Creating some blocks.
-	blocks := make([]database.Block, 0)
-	for i := 0; i < 20; i++ {
-		txOpts := newSignedTx(t)
-		err = state1.UpsertWalletTransaction(txOpts)
-		if err != nil {
-			t.Error(err)
-			t.FailNow()
+	// Let's add 15 blocks to Node1 starting with Nonce 1.
+	var blocks []database.Block
+
+	for i := 1; i <= 15; i++ {
+		tx := database.Tx{
+			ChainID: CHAIN_ID,
+			Nonce:   uint64(i),
+			ToID:    KENNEDY_ACCOUNTID,
+			Value:   1,
+			Tip:     0,
+			Data:    nil,
 		}
 
-		blk, err := state1.MineNewBlock(context.Background())
-		if err != nil {
-			t.Error(err)
-			t.FailNow()
+		signedTx := newSignedTx(tx, JACK_PRIVATEKEY, t)
+		if err := node1.UpsertWalletTransaction(signedTx); err != nil {
+			t.Fatalf("Error upserting wallet transaction: %v", err)
 		}
+
+		blk, err := node1.MineNewBlock(context.Background())
+		if err != nil {
+			t.Fatalf("Error mining new block: %v", err)
+		}
+
 		blocks = append(blocks, blk)
 	}
 
-	// Lets pass them to the 2nd miner, with exception of 2.
-	// This should trigger the ErrChainForked.
+	// Let's add the first 10 blocks to node2, then skip blocks 11 and 12,
+	// then try to add block 13. Remember zero indexing.
 
-	for i, b := range blocks {
-
+	for i, blk := range blocks[:13] {
 		switch {
-		case i == 12:
-			err = state2.ProcessProposedBlock(b)
-			if err == nil {
-				t.Fatal("Should have failed here, fork issue")
-			} else if err == database.ErrChainForked {
-				t.Logf("Failling gracefully due to fork in blocks: %s", err.Error())
-				return
-			} else {
-				t.Fatalf("Ohoh - we gotta an unplanned error: %s", err.Error())
-			}
-		case i != 10 && i != 11:
-			err = state2.ProcessProposedBlock(b)
+		case i < 10:
+			err = node2.ProcessProposedBlock(blk)
 			if err != nil {
-				t.Fatal(err)
+				t.Fatalf("Error proposing new block %d: %v", i, err)
 			}
-		default:
-			t.Log("This is the 10th or 11th, lets skip it and see.")
-		}
 
+		case i == 10 || i == 11:
+			continue
+
+		case i == 12:
+			err := node2.ProcessProposedBlock(blk)
+			if !errors.Is(err, database.ErrChainForked) {
+				t.Fatal("Should have received ErrChainForked error")
+			}
+		}
 	}
 }
 
@@ -120,17 +119,14 @@ func Test_MineAndForceRescynError(t *testing.T) {
 // 2 Miners, mine some blocks on Miner 1, and then, provide the blocks to Miner 2,
 // but one blocks will be missing and we expect to see it raising an error.
 func Test_MineAndForceMissingBlock(t *testing.T) {
-
 	log, err := logger.New("TEST")
 	if err != nil {
-		t.Error(err)
-		t.FailNow()
+		t.Fatalf("Error constructing logger: %v", err)
 	}
 	defer log.Sync()
 
-	state1 := newMiner(t, MINER1_ECDSA)
-
-	state2 := newMiner(t, MINER2_ECDSA)
+	node1 := newNode(MINER1_PRIVATEKEY, t)
+	node2 := newNode(MINER2_PRIVATEKEY, t)
 
 	// Creating the blocks.
 	blocks := make([]database.Block, 0)
@@ -179,66 +175,47 @@ func Test_MineAndForceMissingBlock(t *testing.T) {
 // ================== TOOLKIT FOR TESTS =======================================
 
 const (
-	MINER1_ECDSA   = "8dc79feefd3b86e2f9991def0e5ccd9a5128e104682407b308594bc1032ac7f0"
-	MINER2_ECDSA   = "5aed92a29e1014d83c1d8ac755878723d7e44d8dc129610d11b2022d09ad95bd"
-	MINER3_ECDSA   = "ce07a51ad1d72084aed971b24042f320b4673e852b59eb550375b9eb6747d74a"
-	PERSONA1_ECDSA = "9f332e3700d8fc2446eaf6d15034cf96e0c2745e40353deef032a5dbf1dfed93"
-	PERSONA2_ECDSA = "aed31b6b5a341af8f27e66fb0b7633cf20fc27049e3eb7f6f623a4655b719ebb"
-	PERSONA3_ECDSA = "601d7574860c135e9d3c1d52b0ee997404130edc2a1177c78fda92dd6a3dc2f7"
-	NONCE_ZERO     = uint64(0)
+	MINER1_PRIVATEKEY = "8dc79feefd3b86e2f9991def0e5ccd9a5128e104682407b308594bc1032ac7f0"
+	MINER2_PRIVATEKEY = "5aed92a29e1014d83c1d8ac755878723d7e44d8dc129610d11b2022d09ad95bd"
+	MINER3_PRIVATEKEY = "ce07a51ad1d72084aed971b24042f320b4673e852b59eb550375b9eb6747d74a"
+	JACK_PRIVATEKEY   = "9f332e3700d8fc2446eaf6d15034cf96e0c2745e40353deef032a5dbf1dfed93"
+	JILL_PRIVATEKEY   = "aed31b6b5a341af8f27e66fb0b7633cf20fc27049e3eb7f6f623a4655b719ebb"
+	SAMMY_PRIVATEKEY  = "601d7574860c135e9d3c1d52b0ee997404130edc2a1177c78fda92dd6a3dc2f7"
+
+	KENNEDY_ACCOUNTID = database.AccountID("0xF01813E4B85e178A83e29B8E7bF26BD830a25f32")
+	PAVEL_ACCOUNTID   = database.AccountID("0xdd6B972ffcc631a62CAE1BB9d80b7ff429c8ebA4")
+	CESAR_ACCOUNTID   = database.AccountID("0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76")
+	BABA_ACCOUNTID    = database.AccountID("0x6Fe6CF3c8fF57c58d24BfC869668F48BCbDb3BD9")
+	ED_ACCOUNTID      = database.AccountID("0xa988b1866EaBF72B4c53b592c97aAD8e4b9bDCC0")
+	MINER1_ACCOUNTID  = database.AccountID("0xFef311483Cc040e1A89fb9bb469eeB8A70935EF8")
+	MINER2_ACCOUNTID  = database.AccountID("0xb8Ee4c7ac4ca3269fEc242780D7D960bd6272a61")
+
+	NONCE_ZERO = 0
+	CHAIN_ID   = 1
 )
 
-// Simple worker - required for the proper operation with states,
-// but it does nothing.
-// The production one will work with other goroutines - we dont want this
-// in our test cases.
+// =============================================================================
+
+// noopWorker implements the Worker interface which does nothing.
 type noopWorker struct{}
 
-func (n *noopWorker) Shutdown()                              {}
-func (n *noopWorker) Sync()                                  {}
-func (n *noopWorker) SignalStartMining()                     {}
-func (n *noopWorker) SignalCancelMining()                    {}
-func (n *noopWorker) SignalShareTx(blockTx database.BlockTx) {}
+func (n noopWorker) Shutdown() {}
 
-// signedTxOpts allows us to simplify creating signed TX, when
-// calling createSignedTX.
-type signedTxOpts struct {
-	keyHex string
-	nonce  uint64
-	to     string
-	value  uint64
-	tip    uint64
-	data   []byte
-}
+func (n noopWorker) Sync() {}
 
-// createSignedTX - This is method will encapsulate the creation of a signed transaction.
-func createSignedTX(opts signedTxOpts) (database.SignedTx, error) {
+func (n noopWorker) SignalStartMining() {}
 
-	privateKey, err := crypto.HexToECDSA(opts.keyHex)
-	if err != nil {
-		return database.SignedTx{}, err
-	}
+func (n noopWorker) SignalCancelMining() {}
 
-	toAccount, err := database.ToAccountID(opts.to)
-	if err != nil {
-		return database.SignedTx{}, err
-	}
+func (n noopWorker) SignalShareTx(blockTx database.BlockTx) {}
 
-	const chainID = 1
-	tx, err := database.NewTx(chainID, opts.nonce, toAccount, opts.value, opts.tip, opts.data)
-	if err != nil {
-		return database.SignedTx{}, err
-	}
-
-	return tx.Sign(privateKey)
-
-}
+// =============================================================================
 
 // newGenesis will create a new Genesis to be used later.
 func newGenesis() genesis.Genesis {
 	g := genesis.Genesis{
 		Date:          time.Now().Add(time.Hour * 24 * -365),
-		ChainID:       1,
+		ChainID:       CHAIN_ID,
 		TransPerBlock: 10,
 		Difficulty:    1,
 		MiningReward:  700,
@@ -252,76 +229,50 @@ func newGenesis() genesis.Genesis {
 	return g
 }
 
-// newSignedTx will manage nonces and give us a new transaction with random value
-// at each call.
-var nonce = NONCE_ZERO //Lets seed the nonce
-func newSignedTx(t *testing.T) database.SignedTx {
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-
-	nonce++
-	so := signedTxOpts{
-		keyHex: PERSONA1_ECDSA,
-		nonce:  nonce,
-		to:     "0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76",
-		value:  r1.Uint64(),
-		tip:    0,
-		data:   []byte{},
-	}
-	ret, err := createSignedTX(so)
+// newSignedTx constructs a signed transaction.
+func newSignedTx(tx database.Tx, hexKey string, t *testing.T) database.SignedTx {
+	privateKey, err := crypto.HexToECDSA(hexKey)
 	if err != nil {
-		t.Errorf("Error creating database transaction: %s", err.Error())
-		t.FailNow()
-		return database.SignedTx{}
+		t.Fatalf("Error constructing private key: %v", err)
 	}
 
-	return ret
+	signedTx, err := tx.Sign(privateKey)
+	if err != nil {
+		t.Fatalf("Error signing transaction: %v", err)
+	}
+
+	return signedTx
 }
 
-// newMiner will create an in memory miner.
-func newMiner(t *testing.T, strkey string) *state.State {
-	if strkey == "" {
-		t.Fatalf("please provide a string w an ECDSA key as strkey")
+// newNode will create an in memory miner.
+func newNode(hexKey string, t *testing.T) *state.State {
+	if hexKey == "" {
+		t.Fatalf("Error with hexKey being empty.")
 	}
-	var err error
+
+	privateKey, err := crypto.HexToECDSA(hexKey)
+	if err != nil {
+		t.Fatalf("Error constructing private key: %v", err)
+	}
 
 	storage, err := memory.New()
 	if err != nil {
-		t.Error(err)
-		t.FailNow()
+		t.Fatalf("Unable to construct memory storage: %v", err)
 	}
 
-	key, err := crypto.HexToECDSA(MINER1_ECDSA)
-	if err != nil {
-		t.Error(err)
-		t.FailNow()
-	}
-
-	evts := events.New()
-
-	ev := func(v string, args ...any) {
-		const websocketPrefix = "viewer:"
-
-		s := fmt.Sprintf(v, args...)
-		if strings.HasPrefix(s, websocketPrefix) {
-			evts.Send(s)
-		}
-	}
-
-	ret, err := state.New(state.Config{
-		BeneficiaryID:  database.PublicKeyToAccountID(key.PublicKey),
+	state, err := state.New(state.Config{
+		BeneficiaryID:  database.PublicKeyToAccountID(privateKey.PublicKey),
 		Host:           "http://localhost:9080",
 		Genesis:        newGenesis(),
 		Storage:        storage,
 		SelectStrategy: "Tip",
 		KnownPeers:     peer.NewPeerSet(),
-		EvHandler:      ev,
+		EvHandler:      func(v string, args ...any) {},
 	})
 	if err != nil {
-		t.Error(err)
-		t.FailNow()
+		t.Fatalf("Error constructing node state: %v", err)
 	}
 
-	ret.Worker = &noopWorker{}
-	return ret
+	state.Worker = noopWorker{}
+	return state
 }
